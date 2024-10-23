6f6eb462c78c12f8ba99439fbf7e195bc9f08304
Edo404 <edoardogamurrini@gmail.com>
2024-10-23 11:54:26 +0200

    header

---
+++ src/Header/Header.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import './Header.css'; // Make sure to import your CSS file
+
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false); // State to manage the menu visibility
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <header>
+        <nav className="navbar">
+            <ul className={`nav-items ${isOpen ? 'active' : ''}`}>
+            <li className="tags"><a href="#">Home</a></li>
+            <li className="tags"><a href="#">About Me</a></li>
+            <li className="tags"><a href="#">Projects</a></li>
+            <li className="tags"><a href="#">Contact Me</a></li>
+            </ul>
+            <div className={`hamburger ${isOpen ? 'active' : ''}`} onClick={toggleMenu}>
+            <div className="hamburger-line line1"></div>
+            <div className="hamburger-line line2"></div>
+            <div className="hamburger-line line3"></div>
+            </div>
+        </nav>
+        </header>
+    );
+};
+
+export default Header;
