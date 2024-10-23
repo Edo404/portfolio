814166f325f72f3d71fdce3d7b1bea958abc9bdf
Edo404 <edoardogamurrini@gmail.com>
2024-10-23 13:23:09 +0200

    buttons

--- src/SectionPicture/SectionPicture.jsx
+++ src/SectionPicture/SectionPicture.jsx
@@ -1,23 +1,29 @@
 import React from 'react';
 import './SectionPicture.css'; // Import your CSS file for styling
-//import Picture from "src/assets/Personal.png"
+// import Picture from "src/assets/Personal.png";
 
 const SectionPicture = () => {
     return (
         <section className="section-picture">
             <div className="text-container">
                 <h1 className="greeting">Hey there! I'm Edoardo</h1>
-                <p className="greeting-p">I enjoy creating websites, ones that suit your desires and needs using latest technologies and cleanest design.</p>
+                <p className="greeting-p">
+                    I enjoy creating websites, ones that suit your desires and needs using the latest technologies and cleanest design.
+                </p>
+                <div className="button-container">
+                    <button className="button">Projects</button>
+                    <button className="button">Contact me</button>
+                </div>
             </div>
             <div className="image-container">
                 <img 
-                src="src/assets/Personal.png" // Replace with your image URL
-                alt="Edoardo" 
-                className="picture" 
+                    src="src/assets/Personal.png" // Replace with your image URL
+                    alt="Edoardo" 
+                    className="picture" 
                 />
             </div>
         </section>
-    )
+    );
 };
 
 export default SectionPicture;
